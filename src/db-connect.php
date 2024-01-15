a43a8075953785abd73c6b389d8813aeac5ec8dc
kaede0204 <2201387@s.asojuku.ac.jp>
2024-01-15 13:52:39 +0900

    s

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql215.phy.lolipop.lan';
+    const DBNAME = 'LAA1518099-shop';
+    const USER = 'LAA1518099';
+    const PASS = 'Pass0204';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
