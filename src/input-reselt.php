0806030b5842e07f210b9f48aac246c654739d85
kaede0204 <2201387@s.asojuku.ac.jp>
2024-01-26 14:23:26 +0900

    a

--- src/input-reselt.php
+++ src/input-reselt.php
@@ -8,34 +8,44 @@
 </head>
 <body>
 <?php
-    $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('insert into product(name, pason) values (?, ?)');
-    if(empty($_POST['name'])){
+try {
+    $pdo = new PDO($connect, USER, PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = $pdo->prepare('INSERT INTO product(name, pason) VALUES (?, ?)');
+
+    if (empty($_POST['name'])) {
         echo '商品名を入力してください。';
-    }else if(empty($_POST['pason'])){
+    } elseif (empty($_POST['pason'])) {
         echo '著者を入力してください。';
-    }else if($sql->execute([ $_POST['id'], $_POST['name'], $_POST['pason']])){
-        echo '<font color="red">追加に成功しました。</font>';
-    }else{
-        echo '<font color="red">追加に失敗しました。</font>';
-    }        
+    } else {
+        if ($sql->execute([$_POST['name'], $_POST['pason']])) {
+            echo '<font color="red">追加に成功しました。</font>';
+        } else {
+            echo '<font color="red">追加に失敗しました。</font>';
+        }
+    }
+} catch(PDOException $e) {
+    // エラーメッセージを表示
+    echo "エラー：" . $e->getMessage();
+}
 ?>
     <br><hr><br>
     <table>
         <tr><th>商品番号</th><th>商品名</th><th>著者</th></tr>
 <?php
-    foreach($pdo->query('select * from product') as $row){
-        echo '<tr>';
-        echo '<td>',$row['id'], '</td>';
-        echo '<td>',$row['name'], '</td>';
-        echo '<td>',$row['pason'], '</td>';
-        echo '</tr>';
-        echo "\n";
-    }
+foreach ($pdo->query('SELECT * FROM product') as $row) {
+    echo '<tr>';
+    echo '<td>', $row['id'], '</td>';
+    echo '<td>', $row['name'], '</td>';
+    echo '<td>', $row['pason'], '</td>';
+    echo '</tr>';
+    echo "\n";
+}
 ?>
-</table>
-<form action="input.php" method="post">
-    <button type="submit">追加画面へ戻る</button>
-</form>
+    </table>
+    <form action="input.php" method="post">
+        <button type="submit">追加画面へ戻る</button>
+    </form>
 </body>
 </html>
--- src/input.php
+++ src/input.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset-="UTF-8">
-        <title>final 商品追加</title>
-    </head>
-    <body>
-        <p>商品を追加します</p>
-        <form action="input-reselt.php" method="post">
-            
-            商品名<input type="text" name="name"><br>
-            著者<input type="text" name="pason"><br>
-            <button type="submit">追加</button>
-        </form>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>final 商品追加</title>
+</head>
+<body>
+    <p>商品を追加します</p>
+    <form action="input-reselt.php" method="post">
+        商品名<input type="text" name="name"><br>
+        著者<input type="text" name="pason"><br>
+        <button type="submit">追加</button>
+    </form>
+</body>
 </html>
